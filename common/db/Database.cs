5751e537e6fcd366605aa561f767bc2e0104c6a2
Skilly <32639056+moistosaurus@users.noreply.github.com>
2019-08-10 23:41:12 +0100

    Added logging (NLog) & start on Database stuff

---
+++ common/db/Database.cs
@@ -0,0 +1,34 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace common.db
+{
+    public class Database
+    {
+        public readonly int DatabaseIndex;
+
+        private readonly ConnectionMultiplexer _redis;
+        private readonly IDatabase _db;
+        private readonly IServer _server;
+
+        public IDatabase Conn => _db;
+        public readonly ISubscriber Sub;
+
+        public Database(string host, int port, string password, int dbIndex)
+        {
+            DatabaseIndex = dbIndex;
+            var conString = host + ":" + port + ",syncTimeout=60000";
+            if (!string.IsNullOrWhiteSpace(password))
+                conString += ",password=" + password;
+
+            _redis = ConnectionMultiplexer.Connect(conString);
+            _server = _redis.GetServer(_redis.GetEndPoints(true)[0]);
+            _db = _redis.GetDatabase(DatabaseIndex);
+            Sub = _redis.GetSubscriber();
+        }
+    }
+}
--- world/Program.cs
+++ world/Program.cs
@@ -1,15 +1,37 @@
-﻿using System;
+﻿using common.db;
+using NLog;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace world
 {
     class Program
     {
+        internal static readonly Logger Log = LogManager.GetCurrentClassLogger();
+        internal static readonly ManualResetEvent Shutdown = new ManualResetEvent(false);
+
         static void Main(string[] args)
         {
+            AppDomain.CurrentDomain.UnhandledException += LogUnhandledException;
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            Thread.CurrentThread.Name = "Entry";
+
+            Console.CancelKeyPress += delegate
+            {
+                Shutdown.Set();
+            };
+
+            Shutdown.WaitOne();
+        }
+
+        private static void LogUnhandledException(object sender, UnhandledExceptionEventArgs args)
+        {
+            Log.Fatal((Exception)args.ExceptionObject);
         }
     }
 }
